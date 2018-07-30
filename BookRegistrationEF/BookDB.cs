2bca752e2ec3c4e7015c15eb6274856d245c5f85
JEANNA KAWAMOTO <kawzie30@live.com>
2018-07-30 15:34:35 -0700

    Added two different methods that will both update

--- BookRegistrationEF/BookDB.cs
+++ BookRegistrationEF/BookDB.cs
@@ -27,5 +27,41 @@ namespace BookRegistrationEF
             context.Book.Add(b);
             context.SaveChanges();
         }
+
+        // Ef will track an object if you pull out of database and then do modifcations 
+        public static void Update(Book b)
+        {  // METHOD #1 :
+            var context = new BkRegDBContext();
+
+            // get book from database
+            Book originalBook = context.Book.Find(b.ISBN);
+
+            // update any changed properties twice in database
+            originalBook.Price = b.Price;
+            originalBook.Title = b.Title;
+        }
+
+        public static void UpdateAlternative(Book b)
+        {
+            var context = new BkRegDBContext();
+
+            // add Book object  to current context
+            context.Book.Add(b);
+
+            //Let EF know the book already exists, just modified
+            // can use a using by removing system.data.entity
+            context.Entry(b).State = System.Data.Entity.EntityState.Modified;
+            context.SaveChanges();
+        }
+
+        public static void Delete(Book b)
+        {   // METHOD #1  
+            var context = new BkRegDBContext();
+            context.Book.Add(b);
+
+            // mark the book as deleted 
+            context.Entry(b).State = System.Data.Entity.EntityState.Deleted;
+            context.SaveChanges();
+        }
     }
 }
