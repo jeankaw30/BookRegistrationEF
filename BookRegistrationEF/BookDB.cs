70af0ac3a34382b9ea65bbd1a5c76bbe9fb28c69
JEANNA KAWAMOTO <kawzie30@live.com>
2018-07-30 14:19:32 -0700

    Add project files.

---
+++ BookRegistrationEF/BookDB.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookRegistrationEF
+{
+    public static class BookDB
+    {
+        public static List<Book> GetAllBooks()
+        {
+            BkRegDBContext context = new BkRegDBContext();
+            List<Book> allBooks = 
+                                (
+                                from b in context.Book
+                                select b
+                                ).ToList();
+            return allBooks;
+        }
+
+        public static void Add(Book b)
+        {
+            BkRegDBContext context = new BkRegDBContext();
+
+            // asume book vilid
+            context.Book.Add(b);
+            context.SaveChanges();
+
+        }
+    }
+}
---
+++ BookRegistrationEF/CustomerDB.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookRegistrationEF
+{
+    static class CustomerDB
+    {
+        public static List<Customer> GetAllCustomer()
+        {
+            BkRegDBContext context = new BkRegDBContext();
+            List<Customer> customers = context.Customer.ToList();
+            return customers;
+        }
+
+        public static void Add(Customer c )
+        {
+            BkRegDBContext context = new BkRegDBContext();
+            // assume is valid
+            context.Customer.Add(c);
+            context.SaveChanges();
+        }
+       
+    }
+}
