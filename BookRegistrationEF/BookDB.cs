3b475d0f30b96806a8cfe97837151558bfe91a5a
JEANNA KAWAMOTO <kawzie30@live.com>
2018-07-30 13:45:24 -0700

    created another branch AddBookandCustomers

---
+++ BookRegistrationEF/BookDB.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookRegistrationEF
+{
+    public static class BookDB
+    {
+        public static List<Book> GetAllBooks();
+
+        public static void Add(Book b)
+        {
+            BookContext context = new BookContext();
+
+            // asume book vilid
+            context.Book.Add(b);
+            context.SaveChanges();
+
+        }
+        
+            
+    }
+}
